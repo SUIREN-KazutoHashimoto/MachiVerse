bf5496cade0c70d0bfa309373c27eadbaf44a917
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-07 10:24:18 +0900

    fix: correct Admin management boundary declaration (#74)

--- src/MachiVerse.Administration.View/Modules/Management/ManagementModuleBoundary.cs
+++ src/MachiVerse.Administration.View/Modules/Management/ManagementModuleBoundary.cs
@@ -1,4 +1,6 @@
 namespace MachiVerse.Administration.View.Modules.Management;
 
 // ADMIN-03/04 extension boundary for protocol-confirmed management flows.
-public interface IManagementModuleBoundary;
+public interface IManagementModuleBoundary
+{
+}
