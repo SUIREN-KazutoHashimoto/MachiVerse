87c07c6411b7a2def76587d3b7bd0d71f3257481
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-09-07 10:24:13 +0900

    fix: correct Admin monitoring boundary declaration (#74)

--- src/MachiVerse.Administration.View/Modules/Monitoring/MonitoringModuleBoundary.cs
+++ src/MachiVerse.Administration.View/Modules/Monitoring/MonitoringModuleBoundary.cs
@@ -1,4 +1,6 @@
 namespace MachiVerse.Administration.View.Modules.Monitoring;
 
 // ADMIN-02 extension boundary. No target component internal types are admitted here.
-public interface IMonitoringModuleBoundary;
+public interface IMonitoringModuleBoundary
+{
+}
